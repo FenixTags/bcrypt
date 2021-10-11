c385657d9888d2e89b9d13a4b33ee25f367503d2
FenixTags <josedavid.jdss777@gmail.com>
2021-10-11 09:00:50 -0500

    Pequeños ajustes de funciones en el archivo dehasher_bcrypt.py

--- dehasher_bcrypt.py
+++ dehasher_bcrypt.py
@@ -2,20 +2,14 @@ from itertools import product
 import string
 import bcrypt
 
+CHARACTER = string.ascii_lowercase + string.ascii_uppercase + string.digits
 
-def main():
-    minLen = int(input("Ingresa el minimo de caracteres de la contraseña: "))
-    maxLen = int(input("Ingresa el maximo de caracteres de la contraseña: "))
-    counter = 1
-    character = string.ascii_lowercase+string.ascii_uppercase+string.digits
 
-    passHashed = input("Ingrese el hash a crackear: ")
-    passHashed = passHashed.encode()
+def data_input(counter, minLen, maxLen, passHashed):
     passDehashed = None
-
     print(f"Intento: {counter}", end="")
     for i in range(minLen, maxLen):
-        for j in product(character, repeat=i):
+        for j in product(CHARACTER, repeat=i):
             word = "".join(j)
             word = word.encode()
             if bcrypt.checkpw(word, passHashed):
@@ -25,6 +19,17 @@ def main():
             for x in range(len(str(counter))):
                 print("\b", end="")
             print(f"{counter}", end="")
+    return passDehashed
+
+
+def main():
+    minLen = int(input("Ingresa el minimo de caracteres de la contraseña: "))
+    maxLen = int(input("Ingresa el maximo de caracteres de la contraseña: "))
+    counter = 1
+
+    passHashed = input("Ingrese el hash a crackear: ")
+    passHashed = passHashed.encode()
+    passDehashed = data_input(counter, minLen, maxLen, passHashed)
     print(f"\nLa contraseña deshasheada es: {passDehashed}")
 
 
